037bcd815e21dcb796ffe7d7f024abb7853299e4
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-17 13:37:15 -0400

    Version 2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const sequelize = require('./database/database');
 
 const app = express();
 const port = process.env.PORT || 3000;
