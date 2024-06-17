a2df42b392dded40b69c3c6c01703564d6e94e40
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-17 13:49:57 -0400

    Version 2

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const sequelize = require('./database/database'); // Importa la instancia de Sequelize
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// Rutas API
 app.use('/api', routes);
 
+// Sincronización de la base de datos
+sequelize.sync({ alter: true }) // Opciones: alter, force, etc.
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch(error => {
+    console.error('Error synchronizing database:', error);
+  });
+
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
