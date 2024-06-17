66fa14e57bd06c6f3121873b613f7dc10400fcbb
DereckFernandoBaldiviezoCruz <dereck120120120@gmail.com>
2024-06-17 13:32:43 -0400

    Version 2

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ app.use('/api', routes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+sequelize.sync({ alter: true })
+  .then(() => {
+    console.log('Database synchronized');
+  })
+  .catch(error => {
+    console.error('Error synchronizing database:', error);
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
